d4a74dccff155b98ca8eb0a9502d8cd71aa1e0cf
SmartConnor <merigildod3205@masseyhigh.school.nz>
2026-03-04 14:24:40 +1300

    Add files via upload

---
+++ C_01_Make_Statement.py
@@ -0,0 +1,35 @@
+# Functions go here
+def make_statement(statement, decoration):
+    """Emphasizes headings by adding decoration
+    at the start and end"""
+
+    print(f"{decoration * 3} {statement} {decoration * 3}")
+
+
+# Main Routine goes here
+make_statement("Programming is Fun!", "👍")
+print()
+
+
+# functions go here...
+def yes_no(question):
+    """Checks that users enter yes / y or no / n to a question"""
+
+    while True:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            return "yes"
+        elif response == "no" or response == "n":
+            return "no"
+        else:
+            print("Please enter yes (y) or no (n).\n")
+
+# Main routine goes here
+
+
+# loop for testing purposes...
+while True:
+    want_instructions = yes_no("Do you want to read the instructions? ")
+    print(f"You chose {want_instructions}\n")
+
